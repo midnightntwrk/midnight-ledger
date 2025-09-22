b5b09d328dd676c0c382c2b54b87d3d0ff0e7392
Thomas Kerber <thomas.kerber@iohk.io>
2025-09-22 18:22:43 +0100

    Expose functions on pricing margins.

--- base-crypto/src/cost_model.rs
+++ base-crypto/src/cost_model.rs
@@ -540,6 +540,26 @@ impl FixedPoint {
         }
     }
 
+    /// Raises the number to an integer power.
+    pub fn powi(self, mut exp: i32) -> Self {
+        match exp {
+            i32::MIN..=-1 => dbg!(FixedPoint::ONE / self).powi(dbg!(-exp)),
+            0 => FixedPoint::ONE,
+            1..=i32::MAX => {
+                let mut acc = FixedPoint::ONE;
+                let mut cur = self;
+                while exp >= 1 {
+                    if exp & 0b1 != 0 {
+                        acc = acc * cur;
+                    }
+                    cur = cur * cur;
+                    exp >>= 1;
+                }
+                acc
+            }
+        }
+    }
+
     /// Instantiates a fixed point from a/b (rounded up to the nearest 2^-64)
     ///
     /// Unlike [`FixedPoint::from_u128_div`], this method is `const`, due to using only builtin
@@ -873,6 +893,14 @@ mod tests {
             within_permissible_error(
                 a, b, epsilon
             );
+        }
+        #[test]
+        fn fixed_point_powi(a in (1e-1f64..1e1f64), b in (-15..15)) {
+            if a != 0.0 {
+                let pure_error_factor = a.powi(b) / f64::from(FixedPoint::from(a).powi(b));
+                let non_compounded_error_factor = pure_error_factor.powi(-b.abs());
+                within_permissible_error(1.0, non_compounded_error_factor, 0.001);
+            }
         }
         #[test]
         fn fixed_point_addition(a in (-1e18f64..1e18f64), b in (-1e18f64..1e18f64)) {
