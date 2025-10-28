55079fe41a27d95633911fe932b95ac041a2aadb
Thomas Kerber <thomas.kerber@iohk.io>
2025-10-28 16:17:01 +0000

    Remove special-casing of checks on test-utilities feature.

--- ledger/tests/composable.rs
+++ ledger/tests/composable.rs
@@ -29,7 +29,6 @@ use midnight_ledger::semantics::TransactionResult;
 use midnight_ledger::structure::{
     ContractDeploy, INITIAL_PARAMETERS, ProofPreimageMarker, Transaction,
 };
-#[cfg(feature = "proving")]
 use midnight_ledger::test_utilities::PUBLIC_PARAMS;
 use midnight_ledger::test_utilities::{Resolver, TestState, test_resolver, verifier_key};
 use midnight_ledger::test_utilities::{test_intents, tx_prove};
@@ -51,7 +50,6 @@ use transient_crypto::commitment::PedersenRandomness;
 use transient_crypto::fab::ValueReprAlignedValue;
 use transient_crypto::hash::transient_commit;
 use transient_crypto::proofs::KeyLocation;
-#[cfg(feature = "proving")]
 use transient_crypto::proofs::{ProvingKeyMaterial, Resolver as ResolverT};
 use zswap::Delta;
 use zswap::{Offer, Output, Transient};
@@ -63,7 +61,6 @@ lazy_static! {
     static ref RESOLVER_BURN: Resolver = test_resolver("composable-burn");
 }
 
-#[cfg(feature = "proving")]
 async fn resolve_any(key: KeyLocation) -> std::io::Result<Option<ProvingKeyMaterial>> {
     let resolvers = [
         &*RESOLVER_INNER,
@@ -79,7 +76,6 @@ async fn resolve_any(key: KeyLocation) -> std::io::Result<Option<ProvingKeyMater
     Ok(None)
 }
 
-#[cfg(feature = "proving")]
 lazy_static! {
     static ref RESOLVER: Resolver = Resolver::new(
         PUBLIC_PARAMS.clone(),
@@ -93,11 +89,6 @@ lazy_static! {
         ),
         Box::new(|inp| Box::pin(resolve_any(inp))),
     );
-}
-
-#[cfg(not(feature = "proving"))]
-lazy_static! {
-    static ref RESOLVER: Resolver = ();
 }
 
 fn program_with_results<D: DB>(
@@ -125,8 +116,8 @@ async fn composable() {
 
     // Part 1: Deploy inner
     println!(":: Part 1: Deploy inner");
-    let get_op = ContractOperation::new(verifier_key(&RESOLVER, "get").await);
-    let set_op = ContractOperation::new(verifier_key(&RESOLVER, "set").await);
+    let get_op = ContractOperation::new(dbg!(verifier_key(&RESOLVER, "get").await));
+    let set_op = ContractOperation::new(dbg!(verifier_key(&RESOLVER, "set").await));
     let auth_sk: HashOutput = rng.r#gen();
     let auth_pk = persistent_commit(b"mdn:ex:ci", auth_sk);
     let contract = ContractState::new(
@@ -159,6 +150,7 @@ async fn composable() {
     };
     let mut strictness = WellFormedStrictness::default();
     strictness.enforce_balancing = false;
+    dbg!(&tx);
     state.assert_apply(&tx, strictness);
 
     // Part 2: Deploy outer
