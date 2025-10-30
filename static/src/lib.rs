efd9f11dcc23b6d4eda75521c6fb15a69c5805bb
Michal Rus <michal.rus@iohk.io>
2025-10-30 09:44:55 +0100

    chore: don’t define `midnight_ledger_static::VERSION`

--- static/src/lib.rs
+++ static/src/lib.rs
@@ -2,11 +2,9 @@ use proc_macro::TokenStream;
 use proc_macro2::Literal;
 use quote::quote;
 
-const VERSION: &str = include_str!("../version");
-
 #[proc_macro]
 pub fn version(_input: TokenStream) -> TokenStream {
-    let v = VERSION.trim_end_matches('\n');
+    let v = include_str!("../version").trim_end_matches('\n');
     let lit = Literal::string(v);
     quote!(#lit).into()
 }
