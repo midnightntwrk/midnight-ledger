d6d21ee5ca3029dd65e393d79a5a5d907f679210
Thomas Kerber <thomas.kerber@iohk.io>
2025-12-11 11:11:32 +0000

    Update state divergence identifier script.

--- scripts/state-divergence-identifier.py
+++ scripts/state-divergence-identifier.py
@@ -4,6 +4,7 @@ import sys
 import math
 from collections import defaultdict
 import re
+import difflib
 
 def build_graph(log_path):
     res = defaultdict(list)
@@ -15,55 +16,75 @@ def build_graph(log_path):
                 res[match.group(1)].append(match.group(2))
     return res
 
-def longest_path(graph, src):
-    dists = defaultdict(lambda: (math.inf, None), {src: (0, None)})
-    frontier = [src]
+def longest_path(graph):
+    parents = defaultdict(list)
+    for (node, children) in graph.items():
+        for child in children:
+            parents[child].append(node)
+    genesis_points = [key for key in graph.keys() if len(parents[key]) == 0]
+    dists = defaultdict(lambda: (math.inf, None))
+    for genesis in genesis_points:
+        dists[genesis] = (0, None)
+    frontier = genesis_points
     while len(frontier) != 0:
         curr = frontier.pop()
         next = graph[curr]
         for node in next:
-            dists[node] = (dists[curr][0] + 1, curr)
-            frontier.append(node)
-    end = max(dists.items(), key=lambda x: x[1][0])[0]
+            new_dist = dists[curr][0] + 1
+            if new_dist < dists[node][0]:
+                dists[node] = (new_dist, curr)
+                frontier.append(node)
+    end = max(dists.items(), key=lambda x: x[1][0])
 
     curr = end
     path = []
     while curr:
-        path.append(curr)
-        curr = dists[curr][1]
+        path.append(curr[0])
+        next = curr[1][1]
+        if next:
+            curr = (next, dists[next])
+        else:
+            curr = None
     return list(reversed(path))
 
-def find_divergence(path1, path2):
-    i = 0
-    while i < len(path1) and i < len(path2) and path1[i] == path2[i]:
-        i += 1
-    if i >= len(path1) or i >= len(path2):
-        return None
-    else:
-        return i
+def graph2set(graph):
+    nodeset = []
+    for (key, values) in graph.items():
+        nodeset.append(key)
+        nodeset.extend(values)
+    return set(nodeset)
 
-if len(sys.argv) != 4:
-    print("Usage: scripts/state-divergence-identifier.py <LOGFILE 1> <LOGFILE 2> <GENESIS HASH>")
+def is_disjoint(graph1, graph2):
+    intersection = graph2set(graph1).intersection(graph2set(graph2))
+    return len(intersection) == 0
+
+def graph_size(graph):
+    return len(graph2set(graph))
+
+if len(sys.argv) != 3:
+    print("Usage: scripts/state-divergence-identifier.py <LOGFILE 1> <LOGFILE 2>")
     print("")
     print("This script finds a point where the state evolutions of two logfiles diverge.")
     print("It relies on the ledger 'state transition: <xyz> => <abc>' debug logs,")
     print("building a tree graph for both, finding the longest path in both, and")
     print("finding the point these paths diverge")
 else:
-    hash = sys.argv[3]
     print("parsing log 1...");
     graph1 = build_graph(sys.argv[1])
     print("parsing log 2...");
     graph2 = build_graph(sys.argv[2])
+    if is_disjoint(graph1, graph2):
+        print("graphs are disjoint!")
+        print("total entries: {} / {}".format(graph_size(graph1), graph_size(graph2)))
     print("finding longest paths...")
-    path1 = longest_path(graph1, hash)
-    path2 = longest_path(graph2, hash)
-    divergence_point = find_divergence(path1, path2)
-    if divergence_point == None and len(path1) != len(path2):
-        print("Longest paths do not diverge, but lengths differ: {0} vs {1}".format(len(path1), len(path2)))
-    elif divergence_point == None:
-        print("Longest paths do not diverge")
+    path1 = longest_path(graph1)
+    path2 = longest_path(graph2)
+    (i, j, k) = difflib.SequenceMatcher(a=path1, b=path2).find_longest_match()
+    if k == 0:
+        print("no common substring on longest paths")
     else:
-        print("Longest paths diverge at hash {0}:".format(path1[divergence_point - 1]))
-        print("Going to {0} and {1} respectively".format(path1[divergence_point], path2[divergence_point]))
-        print("Full children at divergence: {0} / {1}".format(graph1[path1[divergence_point - 1]], graph2[path2[divergence_point - 1]]))
+        print("common substring of length {} found".format(k))
+        print("first common state hash: {}".format(path1[i]))
+        print("last common state hash: {}".format(path1[i + k - 1]))
+        print("substring starts at state #{}, ends at state #{} in log 1's longest path of {} states".format(i, i + k - 1, len(path1)))
+        print("substring starts at state #{}, ends at state #{} in log 2's longest path of {} states".format(j, j + k - 1, len(path2)))
