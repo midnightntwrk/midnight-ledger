2cae608bf1719002b48465f90aa82f8420b5a6de
Alex Dalton <alex.dalton@shielded.io>
2026-01-21 12:59:57 +0000

    using patched ledger v6

--- v6-to-v7-state-translation/src/lib.rs
+++ v6-to-v7-state-translation/src/lib.rs
@@ -1,29 +1,26 @@
 use base_crypto::cost_model::CostDuration;
 use std::any::Any;
 use std::io;
-use storage_v7::{arena::Sp, db::DB, state_translation::*};
+use storage::{arena::Sp, db::DB, state_translation::*};
 
 struct LedgerV6ToV7Translation;
 
-impl<D: DB, D2: storage_v6::db::DB>
-    DirectTranslation<
-        ledger_v6::structure::LedgerState<D2>,
-        ledger_v7::structure::LedgerState<D>,
-        D,
-    > for LedgerV6ToV7Translation
+impl<D: DB>
+    DirectTranslation<ledger_v6::structure::LedgerState<D>, ledger_v7::structure::LedgerState<D>, D>
+    for LedgerV6ToV7Translation
 {
     fn required_translations() -> Vec<TranslationId> {
         todo!()
     }
 
     fn child_translations(
-        source: &ledger_v6::structure::LedgerState<D2>,
+        source: &ledger_v6::structure::LedgerState<D>,
     ) -> Vec<(TranslationId, Sp<dyn Any + Send + Sync, D>)> {
         todo!()
     }
 
     fn finalize(
-        source: &ledger_v6::structure::LedgerState<D2>,
+        source: &ledger_v6::structure::LedgerState<D>,
         _limit: &mut CostDuration,
         cache: &TranslationCache<D>,
     ) -> io::Result<Option<ledger_v7::structure::LedgerState<D>>> {
