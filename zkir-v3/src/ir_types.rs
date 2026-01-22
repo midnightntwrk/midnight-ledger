74c320a675c86a27dfe3361b5cc4045de933a934
ambrona <miguel.ambrona@iohk.io>
2026-01-22 15:31:47 +0100

    zkir-v3: type in-circuit memory

--- zkir-v3/src/ir_types.rs
+++ zkir-v3/src/ir_types.rs
@@ -11,10 +11,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use midnight_circuits::types::AssignedNative;
+use midnight_proofs::{circuit::Value, plonk::Error};
 #[cfg(feature = "proptest")]
 use proptest_derive::Arbitrary;
 use serde::{Deserialize, Serialize};
 use serialize::{Deserializable, Serializable, Tagged};
+use transient_crypto::curve::outer;
+
+type F = outer::Scalar;
 
 /// Type of IR values
 #[cfg_attr(feature = "proptest", derive(Arbitrary))]
@@ -24,3 +29,58 @@ pub enum IrType {
     #[serde(rename = "Scalar<BLS12-381>")]
     Native,
 }
+
+/// In-circuit IR value, this is a placeholder for an [IrValue], a circuit
+/// variable that does not necessarily carry actual data (it will carry data
+/// during the proving process, but not during the circuit compilation)
+#[derive(Clone, Debug)]
+pub enum CircuitValue {
+    Native(AssignedNative<F>),
+}
+
+impl CircuitValue {
+    pub fn value(&self) -> Value<F> {
+        match self {
+            CircuitValue::Native(x) => x.value().cloned(),
+        }
+    }
+
+    pub fn get_type(&self) -> IrType {
+        match self {
+            CircuitValue::Native(_) => IrType::Native,
+        }
+    }
+}
+
+/// Implements both `From<T> for Enum` (wrap) and `TryFrom<Enum> for T` (unwrap)
+/// for the specified enum variants.
+macro_rules! impl_enum_from_try_from {
+    ($enum:ident { $($variant:ident => $t:ty),* $(,)? }) => {
+        $(
+            // Wrap: From<T> -> Enum
+            impl From<$t> for $enum {
+                fn from(value: $t) -> Self {
+                    $enum::$variant(value)
+                }
+            }
+
+            // Unwrap: TryFrom<Enum> -> T
+            impl std::convert::TryFrom<$enum> for $t {
+                type Error = Error;
+
+                fn try_from(value: $enum) -> Result<Self, Self::Error> {
+                    match &value {
+                        $enum::$variant(inner) => Ok(inner.clone()),
+                    }
+                }
+            }
+        )*
+    };
+}
+
+// Derives implementations, for every basic type T:
+//  - From<T> for CircuitValue
+//  - TryFrom<CircuitValue> for T
+impl_enum_from_try_from!(CircuitValue {
+    Native => AssignedNative<F>,
+});
