2ba513d851a8ec292a35429cd10b08c42a784a69
Alex Dalton <alex.dalton@shielded.io>
2026-01-27 16:42:59 +0000

    wip

--- v6-to-v7-state-translation/src/lib.rs
+++ v6-to-v7-state-translation/src/lib.rs
@@ -6,41 +6,6 @@ use storage::{arena::Sp, db::DB, state_translation::*, storage::Map};
 struct LedgerV6ToV7Translation;
 
 impl LedgerV6ToV7Translation {
-    fn param_tlid() -> TranslationId {
-        TranslationId(
-            ledger_v6::structure::LedgerParameters::tag(),
-            ledger_v7::structure::LedgerParameters::tag(),
-        )
-    }
-
-    fn bridge_receiving_tlid<D: DB>() -> TranslationId {
-        TranslationId(
-            Map::<coin_structure_v6::coin::UserAddress, u128, D>::tag(),
-            Map::<coin_structure_v7::coin::UserAddress, u128, D>::tag(),
-        )
-    }
-
-    fn unclaimed_block_rewards_tlid<D: DB>() -> TranslationId {
-        TranslationId(
-            Map::<coin_structure_v6::coin::UserAddress, u128, D, ledger_v6::annotation::NightAnn>::tag(),
-            Map::<coin_structure_v7::coin::UserAddress, u128, D, ledger_v7::annotation::NightAnn>::tag()
-        )
-    }
-
-    fn treasury_tlid<D: DB>() -> TranslationId {
-        TranslationId(
-            Map::<coin_structure_v6::coin::TokenType, u128, D>::tag(),
-            Map::<coin_structure_v7::coin::TokenType, u128, D>::tag(),
-        )
-    }
-
-    fn zswap_tlid<D: DB>() -> TranslationId {
-        TranslationId(
-            zswap_v6::ledger::State::<D>::tag(),
-            zswap_v7::ledger::State::<D>::tag(),
-        )
-    }
-
     fn contract_tlid<D: DB>() -> TranslationId {
         TranslationId(
             Map::<
@@ -57,27 +22,6 @@ impl LedgerV6ToV7Translation {
             >::tag(),
         )
     }
-
-    fn utxo_tlid<D: DB>() -> TranslationId {
-        TranslationId(
-            ledger_v6::structure::UtxoState::<D>::tag(),
-            ledger_v7::structure::UtxoState::<D>::tag(),
-        )
-    }
-
-    fn replay_protection_tlid<D: DB>() -> TranslationId {
-        TranslationId(
-            ledger_v6::structure::ReplayProtectionState::<D>::tag(),
-            ledger_v7::structure::ReplayProtectionState::<D>::tag(),
-        )
-    }
-
-    fn dust_tlid<D: DB>() -> TranslationId {
-        TranslationId(
-            ledger_v6::dust::DustState::<D>::tag(),
-            ledger_v7::dust::DustState::<D>::tag(),
-        )
-    }
 }
 
 impl<D: DB>
@@ -85,42 +29,13 @@ impl<D: DB>
     for LedgerV6ToV7Translation
 {
     fn required_translations() -> Vec<TranslationId> {
-        vec![
-            Self::param_tlid(),
-            Self::bridge_receiving_tlid::<D>(),
-            Self::unclaimed_block_rewards_tlid::<D>(),
-            Self::treasury_tlid::<D>(),
-            Self::zswap_tlid::<D>(),
-            Self::contract_tlid::<D>(),
-            Self::utxo_tlid::<D>(),
-            Self::replay_protection_tlid::<D>(),
-            Self::dust_tlid::<D>(),
-        ]
+        vec![Self::contract_tlid::<D>()]
     }
 
     fn child_translations(
         source: &ledger_v6::structure::LedgerState<D>,
     ) -> Vec<(TranslationId, Sp<dyn Any + Send + Sync, D>)> {
-        vec![
-            (Self::param_tlid(), source.parameters.upcast()),
-            (
-                Self::bridge_receiving_tlid::<D>(),
-                source.bridge_receiving.mpt.upcast(),
-            ),
-            (
-                Self::unclaimed_block_rewards_tlid::<D>(),
-                source.unclaimed_block_rewards.mpt.upcast(),
-            ),
-            (Self::treasury_tlid::<D>(), source.treasury.mpt.upcast()),
-            (Self::zswap_tlid::<D>(), source.zswap.upcast()),
-            (Self::contract_tlid::<D>(), source.contract.mpt.upcast()),
-            (Self::utxo_tlid::<D>(), source.utxo.upcast()),
-            (
-                Self::replay_protection_tlid::<D>(),
-                source.replay_protection.upcast(),
-            ),
-            (Self::dust_tlid::<D>(), source.dust.upcast()),
-        ]
+        vec![(Self::contract_tlid::<D>(), source.contract.mpt.upcast())]
     }
 
     fn finalize(
@@ -129,48 +44,10 @@ impl<D: DB>
         cache: &TranslationCache<D>,
     ) -> io::Result<Option<ledger_v7::structure::LedgerState<D>>> {
         let network_id = source.network_id.clone();
-        let Some(parameters) = cache.lookup(&Self::param_tlid(), source.parameters.as_child())
-        else {
-            return Ok(None);
-        };
-        let locked_pool = source.locked_pool;
-        let Some(bridge_receiving) = cache.lookup(
-            &Self::bridge_receiving_tlid::<D>(),
-            source.bridge_receiving.mpt.as_child(),
-        ) else {
-            return Ok(None);
-        };
-        let reserve_pool = source.reserve_pool;
-        let block_reward_pool = source.block_reward_pool;
-        let Some(unclaimed_block_rewards) = cache.lookup(
-            &Self::unclaimed_block_rewards_tlid::<D>(),
-            source.unclaimed_block_rewards.mpt.as_child(),
-        ) else {
-            return Ok(None);
-        };
-        let Some(treasury) =
-            cache.lookup(&Self::treasury_tlid::<D>(), source.treasury.mpt.as_child())
-        else {
-            return Ok(None);
-        };
-        let Some(zswap) = cache.lookup(&Self::zswap_tlid::<D>(), source.zswap.as_child()) else {
-            return Ok(None);
-        };
+        // TODO: recast children
         let Some(contract) =
             cache.lookup(&Self::contract_tlid::<D>(), source.contract.mpt.as_child())
         else {
-            return Ok(None);
-        };
-        let Some(utxo) = cache.lookup(&Self::utxo_tlid::<D>(), source.utxo.as_child()) else {
-            return Ok(None);
-        };
-        let Some(replay_protection) = cache.lookup(
-            &Self::replay_protection_tlid::<D>(),
-            source.replay_protection.as_child(),
-        ) else {
-            return Ok(None);
-        };
-        let Some(dust) = cache.lookup(&Self::dust_tlid::<D>(), source.dust.as_child()) else {
             return Ok(None);
         };
 
@@ -201,6 +78,26 @@ impl<D: DB>
             replay_protection: replay_protection.force_downcast(),
             dust: dust.force_downcast(),
         }))
+    }
+}
+
+// TODO: resurect MPT state translation
+// TODO: contract state translation
+
+struct ContractStateTranslation;
+
+impl<D: DB>
+    DirectTranslation<
+        onchain_state_v6::state::ContractState<D>,
+        onchain_state_v7::state::ContractState<D>,
+        D,
+    > for ContractStateTranslation
+{
+    fn finalize(
+        source: &onchain_state_v6::state::ContractState<D>,
+        limit: &mut CostDuration,
+        cache: &TranslationCache<D>,
+    ) -> io::Result<Option<onchain_state_v6::state::ContractState<D>>> {
     }
 }
 
