d77a8d303b1b745e06d4db96f79653115a3a15ec
Thomas Kerber <thomas.kerber@iohk.io>
2026-02-05 15:11:12 +0000

    Log and emit non-proof-erased tx hashes instead of the misleading proof-erased variant.

--- ledger/src/structure.rs
+++ ledger/src/structure.rs
@@ -1287,12 +1287,15 @@ pub enum Transaction<S: SignatureKind<D>, P: ProofKind<D>, B: Storable<D>, D: DB
 tag_enforcement_test!(Transaction<(), (), Pedersen, InMemoryDB>);
 
 #[derive_where(Debug, Clone)]
-pub struct VerifiedTransaction<D: DB>(pub(crate) Transaction<(), (), Pedersen, D>);
+pub struct VerifiedTransaction<D: DB> {
+    pub(crate) inner: Transaction<(), (), Pedersen, D>,
+    pub(crate) hash: TransactionHash,
+}
 
 impl<D: DB> Deref for VerifiedTransaction<D> {
     type Target = Transaction<(), (), Pedersen, D>;
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.inner
     }
 }
 
