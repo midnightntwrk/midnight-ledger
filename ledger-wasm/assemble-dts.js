d1f2724de4817e43d660af2f3d09fb7db792e2f6
Thomas Kerber <thomas.kerber@iohk.io>
2026-02-06 16:01:55 +0000

    Fix lingering d.ts file mismatches.

--- ledger-wasm/assemble-dts.js
+++ ledger-wasm/assemble-dts.js
@@ -3,4 +3,4 @@ import fs from 'fs';
 
 const features = process.argv.slice(2);
 const data = _.template(fs.readFileSync('ledger-v8.template.d.ts', 'utf8'), { imports: { fs } })({ features });
-fs.writeFileSync('ledger-v7.d.ts', data, 'utf8');
+fs.writeFileSync('ledger-v8.d.ts', data, 'utf8');
