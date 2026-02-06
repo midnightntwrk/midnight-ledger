5d49115e598d884c5a844e18388747626d69606a
Thomas Kerber <thomas.kerber@iohk.io>
2026-02-06 15:26:16 +0000

    Fix assemble-dts file.

--- ledger-wasm/assemble-dts.js
+++ ledger-wasm/assemble-dts.js
@@ -2,5 +2,5 @@ import _ from 'lodash';
 import fs from 'fs';
 
 const features = process.argv.slice(2);
-const data = _.template(fs.readFileSync('ledger-v7.template.d.ts', 'utf8'), { imports: { fs } })({ features });
+const data = _.template(fs.readFileSync('ledger-v8.template.d.ts', 'utf8'), { imports: { fs } })({ features });
 fs.writeFileSync('ledger-v7.d.ts', data, 'utf8');
