0596131ab202186c35472f53878b2d25f9069bfa
ambrona <miguel.ambrona@iohk.io>
2026-03-02 11:20:53 +0100

    zkir-v3: manual (tmp) implementation of [encode]

--- zkir-v3/src/ir_instructions/encode.rs
+++ zkir-v3/src/ir_instructions/encode.rs
@@ -11,13 +11,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use group::ff::{Field, PrimeField};
 use midnight_circuits::{
-    instructions::PublicInputInstructions,
-    types::{AssignedNative, AssignedNativePoint, AssignedScalarOfNativeCurve, Instantiable},
+    instructions::{ArithInstructions, PublicInputInstructions, RangeCheckInstructions},
+    types::{AssignedNative, AssignedNativePoint, Instantiable},
 };
 use midnight_curves::JubjubExtended;
 use midnight_proofs::{circuit::Layouter, plonk::Error};
 use midnight_zk_stdlib::ZkStdLib;
+use num_bigint::BigUint;
 use transient_crypto::curve::Fr;
 
 use crate::{
@@ -31,7 +33,14 @@ pub fn encode_offcircuit(value: &IrValue) -> Vec<IrValue> {
         IrValue::Native(x) => AssignedNative::<F>::as_public_input(&x.0),
         IrValue::JubjubPoint(p) => AssignedNativePoint::<JubjubExtended>::as_public_input(p),
         IrValue::JubjubScalar(s) => {
-            AssignedScalarOfNativeCurve::<JubjubExtended>::as_public_input(s)
+            // TODO: We do not use [AssignedScalarOfNativeCurve::as_public_input]
+            // since that function is not compatible with what we want to do here
+            // (1 JubjubScalar -> 1 Native).
+            //
+            // In future versions of `midnight-zk` we will adopt this very encoding,
+            // but for now we need to do it manually here.
+            debug_assert_eq!(F::NUM_BITS, 255);
+            vec![F::from_bytes_le(&s.to_bytes()).unwrap()]
         }
     };
     encoded
@@ -49,7 +58,40 @@ pub fn encode_incircuit(
     let encoded = match value {
         CircuitValue::Native(x) => std_lib.as_public_input(layouter, x),
         CircuitValue::JubjubPoint(p) => std_lib.jubjub().as_public_input(layouter, p),
-        CircuitValue::JubjubScalar(s) => std_lib.jubjub().as_public_input(layouter, s),
+        CircuitValue::JubjubScalar(s) => {
+            // TODO: We do not simply black-box call [std_lib.jubjub().as_public_input]
+            // since that function is not compatible with what we want to do here
+            // (1 JubjubScalar -> 1 Native).
+            //
+            // In future versions of `midnight-zk` we will adopt this very encoding,
+            // but for now we need to adapt it manually here.
+            debug_assert_eq!(F::NUM_BITS, 255);
+            match std_lib.jubjub().as_public_input(layouter, s)?.as_slice() {
+                [x] => Ok(vec![x.clone()]),
+                [x1, x2] => {
+                    let one = BigUint::from(1u32);
+                    std_lib.assert_lower_than_fixed(layouter, x1, &(&one << 248))?;
+                    std_lib.assert_lower_than_fixed(layouter, x2, &(&one << 4))?;
+                    Ok(vec![std_lib.linear_combination(
+                        layouter,
+                        &[(F::ONE, x1.clone()), (F::from(2).pow([248u64]), x2.clone())],
+                        F::ZERO,
+                    )?])
+                }
+                _ =>
+                // This case is unreachable since the only entry point for assigned
+                // Jubjub scalars that are longer than the canonical 252 bits is
+                // `std_lib.jubjub().scalar_from_le_bytes`, but in ZKIRv3 this is only
+                // called during `decode_incircuit` and always from the 32 bytes of an
+                // `AssignedNative`. Such assigned Jubjub scalar would have fallen in
+                // the previous match case of 2 limbs.
+                {
+                    Err(Error::Synthesis(
+                        "Unexpected number of chunks from as_public_input".into(),
+                    ))
+                }
+            }
+        }
     }?;
     Ok(encoded.into_iter().map(CircuitValue::Native).collect())
 }
