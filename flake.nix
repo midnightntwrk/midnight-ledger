7b2439fb89ce87b79fab94b751f45446669614e3
Alex Dalton <alex.dalton@shielded.io>
2026-03-10 18:19:34 +0000

    reverted toolchain change

--- flake.nix
+++ flake.nix
@@ -490,7 +490,7 @@
           devShells.ci = mkShell {
             inputsFrom = with packages; [ledger];
             packages = [
-              packages.rust-dev-toolchain
+              fenix.packages.${system}.minimal.toolchain
               pkgs.nodejs_22
               pkgs.yarn
               pkgs.jq
