dd4d1354dc0b2501ae486aff4df4bce34c0ae602
Alex Dalton <alex.dalton@shielded.io>
2026-03-27 13:33:09 +0000

    split out integration tests

--- flake.nix
+++ flake.nix
@@ -263,6 +263,16 @@
           ];
           packages.proof-server-version = proof-server-version;
 
+          packages.integration-test-deps = pkgs.symlinkJoin {
+            name = "integration-test-deps";
+            paths = [
+              packages.proof-server
+              packages.onchain-runtime-wasm
+              packages.ledger-wasm
+              packages.zkir-wasm
+            ];
+          };
+
           packages.default = pkgs.symlinkJoin {
             name = "ledger-all";
             paths = [
@@ -272,16 +282,6 @@
               packages.ledger-wasm
               packages.zkir-wasm
               packages.zkir-v3-wasm
-            ];
-          };
-
-          packages.wasm-all = pkgs.symlinkJoin {
-            name = "wasm-all";
-            paths = [
-              packages.ledger-wasm
-              packages.zkir-wasm
-              packages.zkir-v3-wasm
-              packages.onchain-runtime-wasm
             ];
           };
 
