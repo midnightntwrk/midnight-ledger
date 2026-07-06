20d81e59bbc8e3d9250cdbae753d17f9f5438736
Golddy <golddydev@proton.me>
2026-07-06 13:47:47 +0000

    revert: fab decode fix
    Signed-off-by: Golddy <golddydev@proton.me>

--- zkir-v3/src/ir_vm.rs
+++ zkir-v3/src/ir_vm.rs
@@ -165,6 +165,7 @@ fn fab_decode_to_bytes_atom(
             }
             for i in 0..chunks {
                 bytes_from(res, FR_BYTES_STORED, inputs[chunks - 1 - i].clone())?;
+                *inputs = &inputs[1..];
             }
             *inputs = &inputs[chunks..];
             res.extend(res_vec);
